a4f7444dd17da1485601f94a334b57a845e0d6a0
Andrey-Zubachyov <53438644+Andrey-Zubachyov@users.noreply.github.com>
2020-11-14 10:38:46 +0300

    re-create

---
+++ Ruby Project/MyApp/app1.rb
@@ -0,0 +1,15 @@
+#encoding: cp866
+
+print "Are you happy with your salary (Y/N):"
+answ = gets.strip.capitalize
+
+if answ == 'Y'
+	puts 'Very good!'
+end
+
+if answ == 'N'
+   	puts 'Very bad'
+end
+
+
+gets
