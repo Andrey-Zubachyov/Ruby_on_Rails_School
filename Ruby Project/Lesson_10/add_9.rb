d7dbdde70e32ffabf81fb0ec53b60e0dd1cda2f5
Andrey-Zubachyov <53438644+Andrey-Zubachyov@users.noreply.github.com>
2020-12-02 06:51:22 +0300

    re-created

--- Ruby Project/Lesson_10/add_9.rb
+++ Ruby Project/Lesson_10/add_9.rb
@@ -1,10 +1,8 @@
 
 
-arr = [:rock, :paper, :scisors]
-
-
-
 loop do
+
+arr = [:rock, :paper, :scisors]
 
 x = rand(0..2)
 
@@ -14,7 +12,7 @@ print 'Enter your choice (R/P/S):'
 
 choice = gets.strip.capitalize
 
-	break if choice == ''
+#	break if choice == ''
 
 case
 	when	choice == 'R'
@@ -25,6 +23,32 @@ case
 	user_choice = :scisors
 end
 
+matrix = [
+	[:scisors, 	:paper, 	:won_the_computer],
+	[:scisors, 	:rock, 		:the_user_won],
+	[:paper  , 	:scisors, 	:the_user_won],
+	[:paper, 	:rock, 		:won_the_computer],
+	[:rock, 	:paper,		:won_the_computer],
+	[:rock, 	:scisors, 	:the_user_won],
+]
+
+matrix.each do |item|
+	
+	if item[0] == choice_comp && item[1] == user_choice
+		if item[2] ==  :won_the_computer
+			puts "Choice comp: #{choice_comp} // Choice human: #{user_choice} Result: Won the computer."
+		elsif item [2] == :the_user_won
+			puts "Choice comp: #{choice_comp} // Choice human: #{user_choice} Result: The user won."
+		else
+			puts "Choice comp: #{choice_comp} // Choice human: #{user_choice} Result: Drawn game."	
+		end
+	end
+end
+
+end
+
+
+=begin
 case 
 	when choice_comp ==  user_choice 
 		puts "Choice comp: #{choice_comp} // Choice human: #{user_choice} Result: drawn game."
@@ -42,8 +66,9 @@ case
 		puts "Choice comp: #{choice_comp} // Choice human: #{user_choice} Result: Win user."
 
 end
+=end
 
-end
+
 
 
 gets
